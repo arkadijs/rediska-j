56062e2b7a0afecec9d491c5977c52f830d97b54
Arkadi Shishlov <arkadi.shishlov@gmail.com>
2012-08-24 02:23:13 +0300

    Initial import

---
+++ src/main/java/ra/Client.java
@@ -0,0 +1,133 @@
+package ra;
+
+import com.ning.http.client.*;
+import com.ning.http.client.extra.ThrottleRequestFilter;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Future;
+
+/** Full-text search Java API */
+public class Client {
+    private final String baseUri;
+    private final String contentUri;
+    private final AsyncHttpClient http;
+
+    public Client(String host, int port, int concurency, int timeoutMillis) {
+        baseUri = "http://" + host + ":" + port + "/";
+        contentUri = baseUri + "content";
+        http = new AsyncHttpClient(
+                    new AsyncHttpClientConfig.Builder()
+                            .setAllowPoolingConnection(true)
+                            .setFollowRedirects(false)
+                            .setCompressionEnabled(false)
+                            .setConnectionTimeoutInMs(timeoutMillis)
+                            .addRequestFilter(new ThrottleRequestFilter(concurency, timeoutMillis))
+                            .build());
+    }
+
+    /** Represents result of Rediska API call via HTTP. */
+    public class Result {
+        /** The call was sucessfull or not shortcut. */
+        public final boolean success;
+        /** HTTP status code. */
+        public final int code;
+        /** HTTP status text. */
+        public final String message;
+        /** Rediska API time in nano-seconds. */
+        public final long elapsedNanos;
+        /** The list of content-id-s returned by {@link ra.Client#search(String)} */
+        public List<String> ids;
+
+        protected Result(boolean success, int code, String message, long elapsedNanos, List<String> ids) {
+            this.success = success;
+            this.code = code;
+            this.message = message;
+            this.elapsedNanos = elapsedNanos;
+            this.ids = ids != null ? ids : Collections.EMPTY_LIST;
+        }
+
+        protected Result setIds(List<String> ids) {
+            this.ids = ids;
+            return this;
+        }
+    }
+
+    private long elapsed(Response resp) {
+        String hdr = resp.getHeader("X-RA-Elapsed");
+        if (hdr == null)
+            return -1L;
+        return Long.parseLong(hdr);
+    }
+
+    private List<String> parseJsonArray(String str) throws ParseException {
+        return (JSONArray) new JSONParser().parse(str);
+    }
+
+    private Result genericResult(int requiredCode, Response resp) {
+        return new Result(
+                resp.getStatusCode() == requiredCode,
+                resp.getStatusCode(), resp.getStatusText(), elapsed(resp),
+                null
+        );
+    }
+
+    private AsyncCompletionHandler<Result> genericHandler(final int code) {
+        return new AsyncCompletionHandler<Result>() {
+            @Override
+            public Result onCompleted(Response resp) {
+                return genericResult(code, resp);
+            }
+        };
+    }
+    
+    /** Adds content to the search database. */
+    public Future<Result> put(String id, String content) throws IOException {
+        return http.preparePut(contentUri + "/" + id).setBody(content).execute(genericHandler(HttpURLConnection.HTTP_CREATED));
+    }
+
+    /** Searches content for terms in query.
+     * @return Result.ids is a list of content-id-s that matches the query. */
+    public Future<Result> search(String query) throws IOException {
+        return http.prepareGet(contentUri).addQueryParameter("q", query).execute(
+                new AsyncCompletionHandler<Result>() {
+                    @Override
+                    public Result onCompleted(Response resp) throws IOException, ParseException {
+                        return genericResult(HttpURLConnection.HTTP_OK, resp)
+                                .setIds(resp.getStatusCode() == HttpURLConnection.HTTP_OK ? parseJsonArray(resp.getResponseBody()) : null);
+                    }
+                });
+    }
+
+    /** Removes content from database that matches query. */
+    public Future<Result> remove(String query) throws IOException {
+        return remove("q", query);
+    }
+
+    private String join(List<String> list, char sep) {
+        if (list.isEmpty() || (list.size() == 1 && list.get(0).isEmpty()))
+            return "";
+        StringBuilder sb = new StringBuilder();
+        for (String item : list)
+            sb.append(sep).append(item);
+        return sb.toString().substring(1);
+    }
+
+    /** Removes content from database by content-id. */
+    public Future<Result> remove(List<String> ids) throws IOException {
+        return remove("id", join(ids, '|'));
+    }
+
+    private Future<Result> remove(String param, String value) throws IOException {
+        return http.prepareDelete(contentUri).addQueryParameter(param, value).execute(genericHandler(HttpURLConnection.HTTP_OK));
+    }
+
+    public Future<Result> reset() throws IOException {
+        return http.preparePost(baseUri + "reset").execute(genericHandler(HttpURLConnection.HTTP_OK));
+    }
+}
---
+++ src/test/java/ra/ClientTest.java
@@ -0,0 +1,76 @@
+package ra;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+@org.junit.runner.RunWith(org.junit.runners.JUnit4.class)
+public class ClientTest {
+
+    final Client ra = new Client("localhost", 8080, 4, 5000);
+
+    @org.junit.Before
+    public void testReset() throws IOException, ExecutionException, InterruptedException {
+        Future<Client.Result> f = ra.reset();
+        Client.Result r = f.get();
+        assertEquals(200, r.code);
+        System.out.printf("redis-es cleared in %dms%n", r.elapsedNanos / 1000000);
+    }
+
+    private String aboutCat = "Like a black cat in the dark room";
+    private String what = "black cat";
+    private int n = 10;
+
+    @Test
+    public void testPut() throws IOException, ExecutionException, InterruptedException {
+        List<Future<Client.Result>> f = new ArrayList<Future<Client.Result>>(n);
+        for (int i = 0; i < n; ++i)
+            f.add(ra.put("A" + i, aboutCat));
+        for (int i = 0; i < n; ++i)
+            assertEquals(201, f.get(i).get().code);
+    }
+
+    @Test
+    public void testSearch() throws IOException, ExecutionException, InterruptedException {
+        testReset();
+        testPut();
+        Client.Result r = ra.search(what).get();
+        assertEquals(200, r.code);
+        assertEquals(n, r.ids.size());
+    }
+
+    @Test
+    public void testDelete() throws IOException, ExecutionException, InterruptedException {
+        testReset();
+        testPut();
+        assertEquals(200, ra.remove(what).get().code);
+        Client.Result r = ra.search(what).get();
+        assertEquals(200, r.code);
+        assertEquals(0, r.ids.size());
+    }
+
+    @Test
+    public void testDeleteByIds() throws IOException, ExecutionException, InterruptedException {
+        testReset();
+        testPut();
+        Client.Result r = ra.search(what).get();
+        assertEquals(200, r.code);
+        assertEquals(n, r.ids.size());
+        String[] result = r.ids.toArray(new String[n]);
+        Arrays.sort(result);
+        List<String> toRemove = Arrays.asList(Arrays.copyOf(result, n-1));
+        assertEquals(200, ra.remove(toRemove).get().code);
+        Client.Result r2 = ra.search(what).get();
+        assertEquals(200, r2.code);
+        assertEquals(1, r2.ids.size());
+        assertArrayEquals(new String[] { "A" + (n-1) }, r2.ids.toArray());
+    }
+}
